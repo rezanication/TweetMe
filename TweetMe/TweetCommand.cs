4a205e00b5293321875c4dbf0c713462442accd0
Reza Ghaffari <reza-ghaf@hotmail.co.uk>
2018-08-19 18:14:20 +0100

    Add project files.

---
+++ TweetMe/TweetCommand.cs
@@ -0,0 +1,91 @@
+﻿using EnvDTE;
+using Microsoft;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.ComponentModel.Design;
+using Task = System.Threading.Tasks.Task;
+
+namespace TweetMe
+{
+    internal sealed class TweetCommand
+    {
+        public const int CommandId = 0x0100;
+
+        public static readonly Guid CommandSet = new Guid("092c04e1-da08-4830-b0a7-47aaeb808e7a");
+
+        private readonly Package package;
+
+        private TweetCommand(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+        public static TweetCommand Instance
+        {
+            get;
+            private set;
+        }
+
+
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync((typeof(IMenuCommandService))) as OleMenuCommandService;
+            Instance = new TweetCommand(package, commandService);
+        }
+
+        private IServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        private void Execute(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            DTE dte = (DTE)this.ServiceProvider.GetService(typeof(DTE));
+            Assumes.Present(dte);
+            Document activeDocument = dte.ActiveDocument;
+
+            try
+            {
+                if (activeDocument != null && activeDocument?.Type == "Text")
+                {
+                    TextDocument text = (TextDocument)activeDocument.Object(String.Empty);
+                    if (!text.Selection.IsEmpty)
+                    {
+                        if (text.Selection.Text.Length > 140)
+                        {
+                            VsShellUtilities.ShowMessageBox(ServiceProvider, "TweetMe: maximum length is 140 characters", "Oops!", OLEMSGICON.OLEMSGICON_INFO, OLEMSGBUTTON.OLEMSGBUTTON_OK, OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+                        }
+                        else
+                        {
+                            var message = $"{text.Selection.Text}";
+                            System.Diagnostics.Process.Start("https://twitter.com/intent/tweet?text=" + message);
+                        }
+                    }
+                    else
+                    {
+                        VsShellUtilities.ShowMessageBox(ServiceProvider, "You need to select some code!", "Oops!", OLEMSGICON.OLEMSGICON_INFO, OLEMSGBUTTON.OLEMSGBUTTON_OK, OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+                    }
+                }
+            }
+            catch
+            {
+                VsShellUtilities.ShowMessageBox(ServiceProvider, "Something went wrong, try again!", "Oops!", OLEMSGICON.OLEMSGICON_INFO, OLEMSGBUTTON.OLEMSGBUTTON_OK, OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+            }
+
+        }
+
+    }
+}
---
+++ TweetMe/TweetCommandPackage.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.Shell;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Task = System.Threading.Tasks.Task;
+
+namespace TweetMe
+{
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [Guid(TweetCommandPackage.PackageGuidString)]
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
+    public sealed class TweetCommandPackage : AsyncPackage
+    {
+        public const string PackageGuidString = "2b1beedb-c8a1-4960-97b6-ed99a9f133e3";
+
+        public TweetCommandPackage()
+        {
+
+        }
+
+        #region Package Members
+
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            await TweetCommand.InitializeAsync(this);
+        }
+
+        #endregion
+    }
+}
